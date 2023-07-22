b2d944857ccd75b22db5c2bad35318bb6b9b55df
Thomas den Otter <thomasdenotter6@hotmail.com>
2023-07-22 14:42:34 +0200

    Huiswerk klaar

--- les6hw.py
+++ les6hw.py
@@ -4,3 +4,31 @@ print(Auto)
 
 Auto.append("Suzuki")
 print(Auto)
+
+#Opdracht 2
+gegevens ={"Voornaam": "Harry", "Achternaam": "van Winkel", "Geboorte datum": "27-3-1939"}
+print(gegevens)
+
+print(gegevens["Voornaam"])
+
+gegevens["Voornaam"] = "Hendrikus"
+print(gegevens)
+
+#Opdracht3
+a= 10
+b= 2
+c=a/b
+print(f"uitkomst opdracht 3 is {c}")
+
+#Opdracht4
+a=2
+b=3
+c=a**b
+print(f"uitkomst opdracht 4 is {c}")
+
+#Opdracht5
+a=4
+b=5
+c="True"
+if a==b:
+ print(c)
